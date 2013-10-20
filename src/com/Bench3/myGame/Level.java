e58ddacb4fb8d5322261dda2d43fc0d8505f64e9
Bradley Maier <brdlmaier49@gmail.com>
2013-10-20 16:02:53 -0400

    Daniel's game

---
+++ src/com/Bench3/myGame/Level.java
@@ -0,0 +1,12 @@
+package com.Bench3.myGame;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bradley
+ * Date: 10/20/13
+ * Time: 3:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Level {
+    private void
+}
