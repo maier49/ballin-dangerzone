70d58a03160179351ed4869af35bdaf5ab7b0ef6
Bradley Maier <brdlmaier49@gmail.com>
2013-10-20 16:11:52 -0400

    Breakin shit

--- src/com/Bench3/myGame/fdsafds.java
+++ src/com/Bench3/myGame/fdsafds.java
@@ -4,9 +4,8 @@ package com.Bench3.myGame;
  * Created with IntelliJ IDEA.
  * User: Bradley
  * Date: 10/20/13
- * Time: 3:59 PM
+ * Time: 4:09 PM
  * To change this template use File | Settings | File Templates.
  */
-public interface Level {
-    private void fdsafsdl;kafdskl;jasfd
+public class fdsafds {
 }
